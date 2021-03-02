276f34c289d8c68d45e22dc001cc4dfd0733cd7a
talhaaersoy <63413850+talhaaersoy@users.noreply.github.com>
2021-03-02 12:06:38 +0300

    initial commit

---
+++ MultiTenancy/Entities/Concrete/Annal.cs
@@ -0,0 +1,16 @@
+﻿
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Annal : IEntity
+    {
+        public int Id { get; set; }
+        public string Time  { get; set; }
+        public string Category { get; set; }
+        public string Event { get; set; }
+    }
+}
